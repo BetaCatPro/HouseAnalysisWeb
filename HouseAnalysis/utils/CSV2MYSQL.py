f9160650e0b6b8eaf144405fbbbf63cc1b86fe5c
BetaCatPro <zj20162325@163.com>
2020-03-02 09:05:43 +0800

    sql

--- HouseAnalysis/utils/CSV2MYSQL.py
+++ HouseAnalysis/utils/CSV2MYSQL.py
@@ -26,15 +26,7 @@ result=pd.read_sql_query(sql,conn)
 """
 
 df = pd.read_csv("E:\Graduation Project\DataAnalysis\data\jianyang_house.csv", sep=',')
-release_data = df['release_date']
-import time
-# tmp_time = time.strptime(release_data,'%Y年%月m%d日')
-# tmp_time = time.strptime('2019年2月10日','%Y年%m月%d日')
-# year = str(tmp_time.tm_year)
-# month = str(tmp_time.tm_mon)
-# day = str(tmp_time.tm_mday)
-# final_time = year+'-'+month+'-'+day
-# print(final_time)
+
 
 # 将新建的DataFrame储存为MySQL中的数据表，不储存index列
 df.to_sql('house_api', engine, index= False)
